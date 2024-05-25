9525671e695472f97914e96fdcfea43a2dbbe323
bleuarff <bleuarff@gmail.com>
2024-05-25 20:33:11 +0200

    AHT reading/ shorter delay
    Get readings with shorter delay and retries.

--- sketches/libraries/aht20/aht20.cpp
+++ sketches/libraries/aht20/aht20.cpp
@@ -30,7 +30,7 @@ uint8_t AHT20::init(){
     return -2;
   }
 
-  byte status = Wire.read();
+  uint8_t status = Wire.read();
 
   // Calibration required
   if (status != AHT20::STATUS_OK){
@@ -38,37 +38,51 @@ uint8_t AHT20::init(){
     return -3;
   }
 
-  // delay after status before sending command
-  delay(10);
+  delay(10); // delay after init, before sending a read command
   return 0;
 }
 
-// Get readings from sensor and convert to meaningful values
+// Get readings from sensor and compute values
 void AHT20::getValues(float *temp, float *humid){
+
   // measurement trigger command + 2 bytes of parameters
-  byte cmd[3] = { 0xAC, 0x33, 0x00 };
+  uint8_t cmd[3] = { 0xAC, 0x33, 0x00 };
 
   Wire.beginTransmission(AHT20::I2C_ADDR);
   Wire.write(cmd, 3);
   Wire.endTransmission();
 
-  // TODO: shorter wait time & retries
-  delay(200);
+  bool ready = false;
+  do {
+    delay(85); // delay > 80ms recommended
 
-  byte res[7];
-  byte i = 0;
+    // get sensor status
+    Wire.requestFrom(AHT20::I2C_ADDR, (uint8_t)1);
+
+    if (Wire.available()) {
+      uint8_t status = Wire.read();
+      ready = status < 128; // bit[7] at 0 means device is ready.
+    }
+    else
+      Serial.println("Read error");
+  } while (!ready);
+
+  uint8_t res[7];
+  uint8_t i = 0;
   Wire.requestFrom(AHT20::I2C_ADDR, (uint8_t)7);
 
   while (Wire.available()) {
     res[i++] = Wire.read();
   }
 
+  // Compute values from bytes 1-5. First byte is status, last one is CRC (ignored).
+
   // merge bytes to obtain a 20-bit value
   long sh = (res[1] << 12) | (res[2] << 4) | (res[3] >> 4);
-  *humid = (float)sh / 1048576 * 100;
+  *humid = (float)sh / 1048576 * 100; // formula according to datasheet
 
   // merge to 20-bit value
-  byte msb = res[3]<<4; // take lower 4 bits, remove the humidity portion
+  uint8_t msb = res[3]<<4; // take lower 4 bits, remove the humidity portion
   long th = (msb<<12) | (res[4]<<8) | res[5];
-  *temp = (float)th / 1048576 * 200 - 50;
+  *temp = (float)th / 1048576 * 200 - 50; // formula according to datasheet
 }
